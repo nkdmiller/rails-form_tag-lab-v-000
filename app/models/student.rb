deafb85e2d5bd578a423d6bd00f6c45d1ef0ffeb
Nate Miller <natekmiller96@gmail.com>
2018-08-28 18:22:07 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,8 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+  def show
+    @student = Student.find(params[:id])
   def index
     @students = Student.all
   end
