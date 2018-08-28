561c16a2225b56a15a6363c90dcea6fda11c1990
Nate Miller <natekmiller96@gmail.com>
2018-08-28 18:20:10 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,9 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+  def index
+    @students = Student.all
+  end
   def create
     Student.create(first_name: params[:first_name], last_name: params[:last_name])
     redirect_to students_path
